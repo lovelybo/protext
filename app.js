f68e03ebf2a8acaea869bec81bd0b0ca6f43a4ec
lovelybo <m18968045217_1@163.com>
2017-03-01 15:42:46 +0800

    change

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
-var path = require('path')
-var express = require('express')
+//引入path内置模块
+var path = require('path');
+//引入express依赖模块，用来启动静态服务器
+var express = require('express');
+/*console.log(__dirname);*/
+var proxy = require('http-proxy-middleware');
+//实例化express
+var app = express();
 
-var index1 = require('./routes/index')
-var index2 = require('./routes/index')
-var index3 = require('./routes/index')
+app.use('/api',proxy({
+    target:'http://122.10.30.153:9901',
+    pathRewriter:{
+        '^/api':'/'
+    }
+}));
 
-console.log(obj)
-console.log(++index1.val)
-console.log(++index2.val)
-console.log(++index3.val)
-console.log(index1.num())
+//指定访问页面路径
+app.use('/',express.static(path.join(__dirname,'views')));
+app.use('/public',express.static(path.join(__dirname,'public')));
+//指定端口号，建议3000以上,回调函数可以不写
+app.listen(16909,function (){
+    console.log('server run at port 8000')
+});
 
-var app = express()
-
-app.use(express.static(path.join(__dirname, 'app')))
-
-app.get('/', function(req, res){
-    res.send('hello, node')
-})
-
-app.listen(8000, function(){
-    console.log('server run at port 8000');
-})
+//模块导出
+module.expotrs = app;
--- gulpfile.js
+++ gulpfile.js
@@ -16,28 +16,28 @@ var nodemon = require('gulp-nodemon')
 
 gulp.task('stylus',function(){
 //	gulp.src('./stylus/*.styl');
-	return gulp.src('./stylus/**/*.styl')
-//	执行stylus编译
-		.pipe(stylus())
-//		输出编译后的文件
-		.pipe(gulp.dest('./public/css'))
+    return gulp.src('./stylus/**/*.styl')
+    //	执行stylus编译
+        .pipe(stylus())
+        //		输出编译后的文件
+        .pipe(gulp.dest('./public/css'))
 })
 
 
 
 gulp.task('minifycss',['stylus'],function(){
-	return gulp.src('./public/css/**/*.css')  //需要压缩的文件
-			.pipe(minifycss()) 			//执行压缩操作
-			.pipe(gulp.dest('./public/mincss/'))
+    return gulp.src('./public/css/**/*.css')  //需要压缩的文件
+        .pipe(minifycss()) 			//执行压缩操作
+        .pipe(gulp.dest('./public/mincss/'))
 })
 
 gulp.task('watcher',['browserSync','stylus','minifycss'],function(){
-	gulp.watch('./stylus/**/*.styl',['stylus']);
+    gulp.watch('./stylus/**/*.styl',['stylus']);
 //	gulp.watch('./public/js/**/*.js',['uglify']);
-	gulp.watch('./public/mincss/**/*.js',['minifycss']);
-	gulp.watch(['./public/css/**/*.css','./public/js/**/*.js']).on('change',function(){
-		reload();
-	}); 
+    gulp.watch('./public/mincss/**/*.js',['minifycss']);
+    gulp.watch(['./public/css/**/*.css','./public/js/**/*.js']).on('change',function(){
+        reload();
+    });
 })
 
 //gulp.task('uglify',function(){
@@ -46,24 +46,24 @@ gulp.task('watcher',['browserSync','stylus','minifycss'],function(){
 //			.pipe(gulp.dest('./public/minjs/'))
 //})
 gulp.task('nodemon',function(ab){
-	var ft = false;
-	return nodemon({
-		script:'./app.js'
-	}).on('start',function(){
-		if(!ft){
-			ab();
-			tf = true;
-		}
-	})
+    var ft = false;
+    return nodemon({
+        script:'./app.js'
+    }).on('start',function(){
+        if(!ft){
+            ab();
+            tf = true;
+        }
+    })
 })
 
 gulp.task('browserSync',['nodemon'],function(){
-	browserSync.init({
-		proxy:{
-			target:'http://127.0.0.1:8000'
-		},
-		port:8001,
-		open:false,
-		files:['*']
-	})
+    browserSync.init({
+        proxy:{
+            target:'http://127.0.0.1:8000'
+        },
+        port:8001,
+        open:false,
+        files:['*']
+    })
 })
